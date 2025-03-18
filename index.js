1706cc8474fe4f8dab2582e3aced8417b9cd6401
Khaled Ahmed <khaled.ahmed.1022@gmail.com>
2025-03-17 21:21:05 -0300

    mapa level 7

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ const sword = new FloatingSword({
     offset: { x: 30, y: -20 } // Ajuste a posição relativa
 })
 
-let level = 6
+let level = 7
 let levels = {
     1: {
         init: () => {
@@ -348,6 +348,43 @@ let levels = {
                     position: {
                         x: 100,
                         y: 80
+                    },
+                    imageSrc: './img/Elevator Opening (46x56).png',
+                    frameRate: 5,
+                    frameBuffer: 5,
+                    loop: false,
+                    autoplay: false
+                })
+            ]
+            
+
+
+        }
+    },
+    7: {
+        init: () => {
+            parsedCollisions = collisionsLevel7.parse2D()
+            CollisionBlocks = parsedCollisions.createObjectsFrom2D()
+            player.collisionBlocks = CollisionBlocks
+            player.position.x = 100
+            player.position.y = 400
+
+            if (player.currentAnimation)
+                player.currentAnimation.isActive = false
+
+            background = new Sprite({
+                position: {
+                    x: 0,
+                    y: 0
+                },
+                imageSrc: './img/Level-7.png'
+            })
+
+            doors = [
+                new Sprite({
+                    position: {
+                        x: 600,
+                        y: 400
                     },
                     imageSrc: './img/Elevator Opening (46x56).png',
                     frameRate: 5,
